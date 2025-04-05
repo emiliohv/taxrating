46bae79727cc7a2f7e00962e1d14b94cf1333ec5
emiliohv <emiliohv@hotmail.es>
2025-04-05 12:01:15 +0200

    mover navbar a otra línea

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -6,21 +6,19 @@ import Formulario from "./Formulario";
 import AdminLogin from "./AdminLogin";
 import Navbar from "../components/Navbar";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Navbar/>
       <Routes>
+      <Navbar/>
         <Route path="/" element={<Home />} />
+        <Route path="/formulario" element={<Formulario />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/formulario" element={<Formulario />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
-
-
 
