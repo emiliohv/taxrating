d45e50e94fdc248f92e3605ab94177c3759e5815
emiliohv <emiliohv@hotmail.es>
2025-04-05 16:55:41 +0200

    intento con Navbar

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,27 +1,23 @@
-
-import Navbar from "../components/Navbar";
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Admin from "./Admin";
 import Formulario from "./Formulario";
 import AdminLogin from "./AdminLogin";
+import Navbar from "../components/Navbar";
 
 const App = () => {
   return (
     <Router>
-      <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/formulario" element={<Formulario />} />
-        <Route path="/admin" element={<Admin />} />
         <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-      </div>
     </Router>
   );
-}
+};
 
 export default App;
