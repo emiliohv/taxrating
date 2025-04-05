8445a0fe2ac1d9f9f694cc6fa2a665257a72f29b
emiliohv <emiliohv@hotmail.es>
2025-04-05 15:39:33 +0200

    modificaciones admin login

--- frontend/vite.config.cjs
+++ frontend/vite.config.cjs
@@ -1,14 +1,7 @@
-const react = require('@vitejs/plugin-react');
-
-/** @type {import('vite').UserConfig} */
-module.exports = {
-  plugins: [react()],
-};
-
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
 });
-
