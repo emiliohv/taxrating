6082f7a022469167e51796559c3469e0999c6e8e
emiliohv <emiliohv@hotmail.es>
2025-04-05 12:34:55 +0200

    incluir navbar en app

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,5 +1,5 @@
 
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
