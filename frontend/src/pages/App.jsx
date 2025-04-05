2b1ab43599bbbd626f4a78c2f79fa07c51a26d04
emiliohv <emiliohv@hotmail.es>
2025-04-05 11:49:52 +0200

    modificaciones admin login

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-import Formulario from "./pages/Formulario";
-import AdminLogin from "./pages/AdminLogin";
+import Home from "./Home";
+import Admin from "./Admin";
+import Formulario from "./Formulario";
+import AdminLogin from "./AdminLogin";
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/formulario" element={<Formulario />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/formulario" element={<Formulario />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
 
 
