b02be8c1c2605f83b0984a6e2a4790b5957135c7
emiliohv <emiliohv@hotmail.es>
2025-04-09 16:31:24 +0200

    actualización del subtítulo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
       <div className="p-6">
         <div className="text-center mb-6">
           <h1 className="text-3xl font-bold text-blue-700 mb-4">TaxRating</h1>
-          <p className="text-gray-600 text-lg">Valoración objetiva de gestorías y asesorías fiscales, realizada por empleados de la Agencia Tributaria en excedencia</p>
+          <p className="text-gray-600 text-lg">Valoraciones objetivas de gestorías y asesorías fiscales, realizadas por empleados de la Agencia Tributaria en excedencia</p>
         </div>
         <div className="flex flex-wrap justify-between items-center gap-6 mb-6">
           <div className="flex-1 min-w-[200px]">
