158b503aa38f6aa17caee72ddba479a53e6dfbea
emiliohv <emiliohv@hotmail.es>
2025-04-11 06:28:28 +0200

    mas mejoras visuales admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -174,7 +174,8 @@ const Admin = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 bg-gray-100 p-6 rounded mb-6 items-end">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 bg-gray-100 p-6 rounded mb-6 items-end">
+
         <div>
           <label className="block text-sm font-semibold mb-1">Provincia</label>
           <select
