bed51aed5c24ce7aad32d04611c4ac94cc9b6093
emiliohv <emiliohv@hotmail.es>
2025-07-30 23:14:07 +0200

    fix: redirección SPA en Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  //base: './',  👈 Importante para rutas relativas
   build: {
     outDir: 'dist'
   }
