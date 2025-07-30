c22ff03892e2bdd80725fc3005d41d7b4c85b1dc
emiliohv <emiliohv@hotmail.es>
2025-07-30 23:07:50 +0200

    fix: quitar base './' para compatibilidad con rutas en Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  //base: './',  👈 Importante para rutas relativas
   build: {
     outDir: 'dist'
-  },
-  base: './'
+  }
 })
