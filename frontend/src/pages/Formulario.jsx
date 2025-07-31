1352e71a5a0e089d8a75de94e7d2425a82e46b95
emiliohv <emiliohv@hotmail.es>
2025-07-31 19:56:50 +0200

    cambio texto taxrating y subtítulo en Formulario

--- frontend/src/pages/Formulario.jsx
+++ frontend/src/pages/Formulario.jsx
@@ -93,6 +93,7 @@ const Formulario = () => {
   }
 
   return (<>
+  <div className="p-6"></div>
     <div className="text-center mb-6">
       <h1 className="text-3xl font-bold text-blue-700 mb-4">TaxRating</h1>
       <p className="text-gray-600 text-lg">
