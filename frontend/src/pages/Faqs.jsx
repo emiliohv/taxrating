c0bf391107cbcd7e620dc0eb75406bfb8b9d91d1
emiliohv <emiliohv@hotmail.es>
2025-08-03 08:50:30 +0200

    cambio título faqs

--- frontend/src/pages/Faqs.jsx
+++ frontend/src/pages/Faqs.jsx
@@ -122,7 +122,7 @@ const Faqs = () => {
       </p>
     </div>
     <div className="max-w-4xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-3 text-center">Preguntas Frecuentes</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Preguntas Frecuentes</h1>
 
       <div className="flex justify-center gap-4 mb-6">
         <button
