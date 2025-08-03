50ba834ca487fe9d04f4a78949b33a3bf3954032
emiliohv <emiliohv@hotmail.es>
2025-08-03 08:39:48 +0200

    volvemos a atras

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 rutas absolutas correctas
   build: {
-    outDir: '../dist'
-  },
-  base: '/'
-})
+    outDir: 'dist'
+  }
+});
